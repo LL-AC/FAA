cbe4354e38341bcaa846243f85a76f6d14c567c6
LL-AC <142487885+LL-AC@users.noreply.github.com>
2026-07-07 19:17:24 +0800

    Update test_tmed.sh

--- script/tmed2/test_tmed.sh
+++ script/tmed2/test_tmed.sh
@@ -17,4 +17,4 @@ echo $fold
 
 CUDA_VISIBLE_DEVICES=$cuda torchrun --nproc_per_node $node --master_port 12346  main.py --epochs $epochs  --dataset tmed2 --data_path $data_path \
  --batch_size $batch_size --accum_iter 1 --log_dir "./logger/"$logger"/"$fold"/log" --output_dir "./logger/"$logger"/"$fold --pin_memory --model $model --lr $lr \
- --warmup_epochs 2 --weight_decay 0.005 --ctiterion $loss --evaluate --resume "./logger/"$logger"/"$fold"/checkpoint-"$mode".pth" --config_path "$config" --num_classes 3
+ --warmup_epochs 2 --weight_decay 0.005 --ctiterion $loss --evaluate --resume "fold"$fold".pth" --config_path "$config" --num_classes 3
