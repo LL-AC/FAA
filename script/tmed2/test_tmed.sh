1f2cb0d6b373108440c4c7df3cd0133547308cc5
LL-AC <142487885+LL-AC@users.noreply.github.com>
2026-07-07 18:11:47 +0800

    Add files via upload

--- script/tmed2/test_tmed.sh
+++ script/tmed2/test_tmed.sh
@@ -6,13 +6,15 @@ batch_size=$5
 model=$6
 lr=$7
 loss=$8
-fold=$9
-logger=${10}
-config=${11}
-
+mode=$9
+fold=${10}
+logger=${11}
+config=${12}
 
 cd ../..
 
+echo $fold
+
 CUDA_VISIBLE_DEVICES=$cuda torchrun --nproc_per_node $node --master_port 12346  main.py --epochs $epochs  --dataset tmed2 --data_path $data_path \
  --batch_size $batch_size --accum_iter 1 --log_dir "./logger/"$logger"/"$fold"/log" --output_dir "./logger/"$logger"/"$fold --pin_memory --model $model --lr $lr \
- --warmup_epochs 2 --weight_decay 0.005 --ctiterion $loss --config_path "$config" --save_only_model clip_model,text_encoder --num_classes 3 --ema \
+ --warmup_epochs 2 --weight_decay 0.005 --ctiterion $loss --evaluate --resume "./logger/"$logger"/"$fold"/checkpoint-"$mode".pth" --config_path "$config" --num_classes 3
